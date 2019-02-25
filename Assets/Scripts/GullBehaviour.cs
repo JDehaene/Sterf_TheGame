8a1a28bcaaf03e39c6035512a62e5f119bdf7ed0
JDehaene <43540756+JDehaene@users.noreply.github.com>
2019-02-25 15:33:58 +0100

    Initial commit

---
+++ Assets/Scripts/GullBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GullBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private float _flightSpeed;
+    [SerializeField]
+    private float _descendSpeed;
+    [SerializeField]
+    private float _ascendSpeed;
+    [SerializeField]
+    private float _turnSpeed;
+
+    private Camera _cam;
+    private CharacterController _c;
+  
+	void Start ()
+    {
+        _c = GetComponent<CharacterController>();
+        _cam = GetComponent<Camera>();
+	}
+
+	void Update ()
+    {
+        _c.Move(_velocity);
+	}
+}
