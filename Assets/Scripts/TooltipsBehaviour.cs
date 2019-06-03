6fed2fa831ce36f58970a576ed6046aa268b7f25
Faust Messely <faust.messely@student.howest.be>
2019-06-03 23:06:32 +0200

    Ge meugt ton buildn wi zopschete

---
+++ Assets/Scripts/TooltipsBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipsBehaviour : MonoBehaviour {
+
+    private bool _toonKeer = false;
+    public GameObject Tooltip;
+
+	void Update () {
+        if (_toonKeer == true)
+        {
+            Tooltip.SetActive(true);
+        }
+        else
+        {
+            Tooltip.SetActive(false);
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _toonKeer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _toonKeer = false;
+        }
+    }
+}
